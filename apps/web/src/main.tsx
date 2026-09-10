52acc69898bea027a656845adeb390f6b794b928
Putra Dwi Nanda Prijambodo <54822924+putradwinandap@users.noreply.github.com>
2026-09-10 18:05:42 +0700

    feat: connect budget UI to household workspace

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
+import { BudgetMount } from './BudgetMount';
 import './styles.css';
 
 registerSW({ immediate: true });
@@ -9,5 +10,6 @@ registerSW({ immediate: true });
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <BudgetMount />
   </React.StrictMode>
 );
