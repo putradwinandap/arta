1634278a9c2e5fe14eddfc21bd2dc37871001f7b
Putra Dwi Nanda Prijambodo <54822924+putradwinandap@users.noreply.github.com>
2026-09-11 15:37:30 +0700

    feat: require authentication for PWA workflows

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
+import { AuthGate } from './AuthGate';
 import { HouseholdOverviewMount } from './HouseholdOverviewMount';
 import { BudgetMount } from './BudgetMount';
 import { GoalMount } from './GoalMount';
 import { ReconciliationMount } from './ReconciliationMount';
 import { BackupRestoreMount } from './BackupRestoreMount';
 import './styles.css';
+
 registerSW({ immediate: true });
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/><HouseholdOverviewMount/><BudgetMount/><GoalMount/><ReconciliationMount/><BackupRestoreMount/></React.StrictMode>);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthGate>
+      <App />
+      <HouseholdOverviewMount />
+      <BudgetMount />
+      <GoalMount />
+      <ReconciliationMount />
+      <BackupRestoreMount />
+    </AuthGate>
+  </React.StrictMode>,
+);
